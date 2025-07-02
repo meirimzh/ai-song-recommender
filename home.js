bac7ed15e28f9115d6f75f06fe0f3a51c37c2ffd
EstherC <esthercho4227@gmail.com>
2025-07-02 13:58:35 -0700

    Add UI and project files

---
+++ home.js
@@ -0,0 +1,6 @@
+
+
+startButton.onclick=function(){ //=function is assigning an anonymous function you can also just create a function instead of writing one line 
+    window.location.href="userChoice.html";
+
+}
---
+++ userChoice.js
@@ -0,0 +1,4 @@
+backButton.onclick=function() {
+    //window.location is for going to a different page, you can also do window.history.back()
+    window.location.href="home.html";
+}
